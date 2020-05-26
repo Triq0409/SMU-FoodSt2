b94c4b0ba4b8cccda1d51db5de1cd212e71d8c5f
tqdbi <lyoujinn@naver.com>
2020-05-26 11:58:04 +0900

    naver api

---
+++ src/Naver_test/NaverMaps.js
@@ -0,0 +1,34 @@
+import { NaverMap, Marker } from 'react-naver-maps';
+import React, { Component } from 'react';
+
+
+class MyMap extends Component {
+
+    render(){
+        const navermaps = window.naver.maps;
+        return (
+            <NaverMap
+                id='map-sample'
+                style={{
+                    width: '100%',
+                    height: '85vh' 
+                }}
+                defaultZoom={17}
+                defaultCenter={{ lat: 37.5450416, lng: 126.9647459}}
+            >
+            <Marker
+                key={1}
+                position={new navermaps.LatLng(37.5450416, 126.9647459 )}
+                animation={0}
+                onClick={() => {alert('여기는 숙명여대입니다.');}}
+            />    
+            </NaverMap>
+        )
+    }
+}
+
+export default MyMap;
+
+/* <Marker
+position={new naver.maps.LatLng(37.5450416, 126.9647459)}
+/>  */
