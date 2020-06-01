1dd15c5f44820919b58b4ad23dc435ab6298eed9
tqdbi <lyoujinn@naver.com>
2020-06-01 14:52:38 +0900

    css update and NaverMap.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,14 @@ function App() {
       <div className="header">
         <img className="icon" src="http://www.sookmyung.ac.kr/sites/sookmyungkr/images/sub/contents/trade_character_05.png" alt="logo"></img>
           <div className="title">
+          <a href="http://www.naver.com">
             <h4 className="fontSub">송&nbsp;이&nbsp;들&nbsp;의&nbsp; &nbsp;맛&nbsp;집&nbsp;가&nbsp;이&nbsp;드</h4><br /><br />
             <h1 className="font">숙플레이스</h1>
+          </a>
           </div>
       </div>
         <div className="content">
-          <div className="map">
+          <div>
             <MyMap />
           </div>
           Review 공간입니다
--- src/Components/NaverMaps.js
+++ src/Components/NaverMaps.js
@@ -1,42 +1,57 @@
 import { NaverMap, Marker } from 'react-naver-maps';
-import React, { Component } from 'react';
-import '../Naver_test/Form.css'
+import React, { Component, useState } from 'react';
+import '../css/App.css'
 
 /*
     funtion 1 = 버튼 클릭을 확인해 동일한 이름의 마커를 리턴해주는 함수
     funtion 2 = 리스트의 마커를 생성하는 함수
 */
 
-function newMarker() {
-    const cafeList = [
-        ['커피니 숙대점', 37.545162, 126.966798],
-        ['카페코지 숙대점', 37.544918, 126.968947],
-        ['을의 커피', 37.545766, 126.966821]
-    ];
+function btnClicked(){
 
-    const foodList = [
-        ['마시앤바시', 37.544574, 126.968097],
-        ['작은도쿄', 37.543933, 126.970556],
-        ['타타미', 37.544965, 126.968188],
-        ['김성천스시', 37.544211, 126.972588],
-        ['맘스터치', 37.545136, 126.967186],
-        ['버거인', 37.543783, 126.969991],
-        ['서브웨이 숙대점', 37.544757, 126.968130],
-        ['신내떡', 37.544538, 126.969930],
-        ['포36거리(포돈)', 37.545134, 126.966155]
-    ];
 
-    const dessertList = [
-        ['몬스터 플레이스', 37.544345, 126.966821],
-        ['디어파인', 37.544945, 126.966474]
-    ]
-
+    return(
+        <div>test</div>
+    );
 }
 
-
-
 class MyMap extends Component {
+    state ={
+        listName: undefined
+    }
 
+    newMarker() {
+        const cafeList = [
+            ['커피니 숙대점', 37.545162, 126.966798],
+            ['카페코지 숙대점', 37.544918, 126.968947],
+            ['을의 커피', 37.545766, 126.966821]
+        ];
+    
+        const foodList = [
+            ['마시앤바시', 37.544574, 126.968097],
+            ['작은도쿄', 37.543933, 126.970556],
+            ['타타미', 37.544965, 126.968188],
+            ['김성천스시', 37.544211, 126.972588],
+            ['맘스터치', 37.545136, 126.967186],
+            ['버거인', 37.543783, 126.969991],
+            ['서브웨이 숙대점', 37.544757, 126.968130],
+            ['신내떡', 37.544538, 126.969930],
+            ['포36거리(포돈)', 37.545134, 126.966155]
+        ];
+    
+        const dessertList = [
+            ['몬스터 플레이스', 37.544345, 126.966821],
+            ['디어파인', 37.544945, 126.966474]
+        ]
+    
+    }
+
+    getListname() {
+        return(
+            <div>{this.listName}</div>
+        )
+    }
+    
     render(){
         const navermaps = window.naver.maps;
         const locations =[
@@ -65,17 +80,28 @@ class MyMap extends Component {
             )
         );
         return (
-            <NaverMap
-                mapDivId={'maps'}
-                style={{
-                width: '100%',
-                height: '100%' 
-                }}
-                defaultCenter={{  lat: 37.5450416, lng: 126.9647459 }}
-                defaultZoom={15}
-            >
-                {loList}
-          </NaverMap>
+            <div>
+                <div>
+                    <button className='listBtn' onClick={() => this.listName='cafeList'}>카페</button>
+                    <button className='listBtn' onClick={() => this.listName='foodList'}>식당</button>
+                    <button className='listBtn' onClick={() => this.listName='dessertList'}>디저트</button>
+                    <button className='listBtn' onClick={() => alert(this.listName)}>출력</button>
+                    <div>불러올 리스트의 이름은 {this.props.listName}입니다!</div>
+                </div>
+                <div className='map'>
+                    <NaverMap
+                        mapDivId={'maps'}
+                        style={{
+                        width: '100%',
+                        height: '100%' 
+                        }}
+                        defaultCenter={{  lat: 37.5450416, lng: 126.9647459 }}
+                        defaultZoom={15}
+                    >
+                        {loList}
+                </NaverMap>
+                </div>
+          </div>
         )
     }
 }
